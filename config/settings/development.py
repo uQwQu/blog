10f0bd7f8eeab8e208979bd86c0f48f57890edaa
uQwQu <fmrwe@proton.me>
2024-12-13 17:14:00 +0300

    chore: setup basic django project

---
+++ config/settings/development.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
